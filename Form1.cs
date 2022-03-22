a796e62f4713b562ed5dd1fb18e38419fbf99ba4
yusufemiral <100554355+yusufemiral@users.noreply.github.com>
2022-03-22 12:05:15 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kompleks_veri_bağlama_uygulaması_form_tasarımı_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
